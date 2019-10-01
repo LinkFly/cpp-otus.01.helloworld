24bf3a4fb7c3e039c7f3712b5865fa21d2d6373a
linkfly <s.katrevich@lingualeo.com>
2019-10-02 02:03:27 +0300

    correct output text for tz

--- main.cpp
+++ main.cpp
@@ -1,11 +1,13 @@
 #include "lib.h"
 
-#include <spdlog/spdlog.h>
+#include <iostream>
+
+using namespace std;
 
 int main()
 {
-    auto logger = spdlog::stdout_logger_mt("console");
-    logger->info("version {} was started", version());
-
+	cout
+		<< "build " << version() << endl
+		<< "Hello, World!";
     return 0;
 }
